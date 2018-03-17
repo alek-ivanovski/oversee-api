50103a3ee3f2513cbcb60605bff875d10c89b44b
Alek Ivanovski <alek.ivanovski96@gmail.com>
2018-03-17 16:25:33 +0100

    initial commit

---
+++ src/main/java/com/oversee/overseeapi/OverseeApiApplication.java
@@ -0,0 +1,12 @@
+package com.oversee.overseeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OverseeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OverseeApiApplication.class, args);
+	}
+}
